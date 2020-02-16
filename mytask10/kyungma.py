710c68fc85e2f639f488628445da8e504c8023bc
git_first_c <contravia@naver.com>
2020-02-16 19:38:15 +0900

    200216_np

--- mytask10/kyungma.py
+++ mytask10/kyungma.py
@@ -6,3 +6,4 @@ print(root)
 for child in root:
     print(child.tag, child.attrib)
 
+#http://www.garak.co.kr/publicdata/dataOpen.do?id=2697&passwd=Xptmxm100!!&dataid=data4&pagesize=10&pageidx=1&portal.templet=false&p_ymd=20200213&p_jymd=20200212&d_cd=2&p_jjymd=20190213&p_pos_gubun=1&pum_nm=귤
