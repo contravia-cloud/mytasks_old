9025367c96f6f0b671421bbc8ade83f0d07ec214
contravia-cloud <contravia@naver.com>
2020-03-08 23:57:44 +0900

    Merge branch 'master' of https://github.com/contravia-cloud/mytasks
    # Conflicts:
    #	mytask1/test1.py
    #	mytask1/test_myproject1.py

--- mytask10_kyungma/kyungmea2.py
+++ mytask10_kyungma/kyungmea2.py
@@ -9,7 +9,7 @@ params = {'id': '2697',
           'pagesize': '10',
           'pageidx': '1',
           'portal.templet' : 'false',
-          'p_ymd': '20200213',
+          'p_ymd': '20200214',
           'p_jymd': '20200212',
           'd_cd': '2',
           'p_jjymd': '20190213',
@@ -21,10 +21,28 @@ params = {'id': '2697',
 res = requests.get(URL, params=params)
 tree = ElementTree.fromstring(res.content)
 # tree = elemTree.parse('./dataOpen.xml')
+
+f = open("./data_list.txt", 'a')
 for list_tag in tree.findall('./list'):
-    grade_tag = list_tag.find('G_NAME_A')
-    unit_tag = list_tag.find('U_NAME')
-    if grade_tag.text == '상' and unit_tag.text == '   3키로상자':
-        print(grade_tag.text)
-        avr_price_tag = list_tag.find('AV_P_A')
-        print(avr_price_tag.text)
+    child_tags = list_tag.findall('*')
+    data = ''
+    for child_tag in child_tags:
+        data = data + '\t' + child_tag.text
+    data = data + '\n'
+    f.write(data)
+f.write('\n')
+f.close()
+
+
+
+
+
+
+    # grade_tag = list_tag.find('G_NAME_A')
+    # unit_tag = list_tag.find('U_NAME')
+    # if grade_tag.text == '상' and unit_tag.text == '   3키로상자':
+    #     print(grade_tag.text)
+    #     avr_price_tag = list_tag.find('AV_P_A')
+    #     print(avr_price_tag.text)
+
+
